a0113fc4acf4228cacc141b3e5952f45bd09fd7d
SwimChamp2014 <singhaanya06@gmail.com>
2024-09-07 10:47:39 -0700

    Homework

---
+++ birthday_dates.py
@@ -0,0 +1,15 @@
+bday1 = input("What date is your birthday? ")
+
+bday2 = input("What date is your birthday? ")
+
+bday3 = input("What date is your birthday? ")
+
+bday4 = input("What date is your birthday? ")
+
+bday5 = input("What date is your birthday? ")
+
+print (bday1, " ")
+print (bday2, " ")
+print (bday3, " ")
+print (bday4, " ")
+print (bday5, " ")

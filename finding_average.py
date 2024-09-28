dd8a87c5661cb8a427bc62da5c994d58d69acd4d
SwimChamp2014 <singhaanya06@gmail.com>
2024-09-28 09:32:40 -0700

    d

---
+++ finding_average.py
@@ -0,0 +1,14 @@
+#11,98, 94, 41, 96
+
+tree1=11
+tree2=98
+tree3=94
+tree4=41
+tree5=96
+
+#finding the sum
+trees_length_combined = tree1 + tree2 + tree3 + tree4 + tree5
+
+#finding average length of the trees
+trees_average_length=trees_length_combined/5
+print("The tree's average length is",trees_average_length )

32a9793c8053d615c1c9e314ee07449784b8cfce
SwimChamp2014 <singhaanya06@gmail.com>
2024-10-19 09:15:57 -0700

    d

---
+++ and_orlesson.py
@@ -0,0 +1,19 @@
+a=5
+b=15
+c=-5
+
+if a and b and c == True: 
+    print("These numbers have a boolean value of true")
+
+else:
+    print("These numbers have a boolean value of False")
+
+a=6
+b=3
+c=-2
+
+if a > 0 or b>0:
+    print("One of these numbers is greater than 0")
+
+else:
+    print("No number is greater than zero")

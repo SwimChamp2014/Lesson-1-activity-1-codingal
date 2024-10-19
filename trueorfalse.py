1031ec9a86d07d6ef1503d37825532f4c15059fc
SwimChamp2014 <singhaanya06@gmail.com>
2024-10-19 09:29:04 -0700

    d

---
+++ trueorfalse.py
@@ -0,0 +1,32 @@
+a=5
+b=5
+c=6
+
+print(a != b)
+print(b != c)
+
+a = "chess"
+b= "swimming"
+
+if a != b:
+    print(a, "and", b, "are different.")
+
+else:
+    print(a, "and", b, "are the same")
+
+a=4
+b=10
+
+if (a==1) != (b == 10):
+    print("hello")
+
+else:
+    print("bye")
+
+x =int(input("enter a number: "))
+
+if x % 2 != 0:
+    print("the number is odd")
+
+else:
+    print("The number is even")

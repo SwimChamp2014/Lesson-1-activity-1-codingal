c364ae6c481c5f1d0bb580b3909410507cfe2d4d
SwimChamp2014 <singhaanya06@gmail.com>
2024-10-19 08:17:03 -0700

    d

---
+++ clothestowear.py
@@ -0,0 +1,10 @@
+temperature = int(input("What is the temperature(in Fahrenheit): "))
+
+if temperature > 60:
+    print("Wear a t-shirt and a short")
+
+if temperature < 60:
+    print("Wear a hoodie and pants")
+
+else:
+    print("It is your choice. You can choose between a t-shirt and shorts or a hoodie and pants")

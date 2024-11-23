e2878f638cb75782b921adc8d1b6ce86574503fe
SwimChamp2014 <singhaanya06@gmail.com>
2024-11-22 20:20:32 -0800

    d

---
+++ isageinbetween10and20.py
@@ -0,0 +1,8 @@
+#Write a program to check if the user's age is between 10 and 20 or not
+
+age = int(input("Enter your age: "))
+
+if age > 10 and age < 20:
+    print("Your age is in between 10 and 20")
+else:
+    print("Your age is not in between 10 and 20")

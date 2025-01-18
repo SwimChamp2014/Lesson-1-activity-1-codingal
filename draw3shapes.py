3a46a81fb8df40e1c3488c8b4686b50a37f079df
SwimChamp2014 <singhaanya06@gmail.com>
2025-01-18 08:11:14 -0800

    d

---
+++ draw3shapes.py
@@ -0,0 +1,32 @@
+import turtle
+turtle.Screen().bgcolor("blue")
+turtle.Screen().setup(400, 400)
+pen = turtle.Turtle()
+
+
+shape = input("Which shape do you want to draw [hexagon, square, pentagon]: ")
+
+if shape == "pentagon" or "Pentagon":
+    side_length = int(input("What is the side length that you want: "))
+    num_sides = 5
+    angle = 360/num_sides
+    pen.begin_fill()
+
+    for i in range (num_sides):
+        pen.forward(side_length)
+        pen.right(angle)
+
+    pen.end_fill()
+
+elif shape == "hexagon" or "Hexagon":
+    num_sides = 6
+    side_length = int(input("Enter the side length you want: "))
+    angle = 360/num_sides
+    pen.begin_fill()
+
+    for i in range(num_sides):
+        pen.forward(side_length)
+        pen.right(angle)
+
+    pen.end_fill()
+# How come when I run this and type in hexagon it still draws a pentagon

a83fdacfa98c47241632fd4c7d4e8be0698b4ace
SwimChamp2014 <singhaanya06@gmail.com>
2025-05-24 12:40:25 -0700

    d

---
+++ h12.py
@@ -0,0 +1,5 @@
+import math
+
+my_tuple = (2, 3, 4)
+result = math.prod(my_tuple)  # Directly calculate the product
+print("The product of the {} is {}".format(my_tuple, result))
--- tuple1.py
+++ tuple1.py
@@ -13,9 +13,4 @@ r = (1,2,3,3,2,1)
 if (palind(r)):
     print("The Tuple is Flip-Flop")
 else:
-    print("The Tuple is not Flip-Flop
-    
-    
-    
-    " \
-    "")
+    print("The Tuple is not Flip-Flop")

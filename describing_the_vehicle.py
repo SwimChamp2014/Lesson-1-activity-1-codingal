96ebab1ea7931eaaf62fe8d6a7a165fb79472402
SwimChamp2014 <singhaanya06@gmail.com>
2025-09-20 08:15:52 -0700

    f

---
+++ describing_the_vehicle.py
@@ -0,0 +1,20 @@
+class BMW():
+    def cost():
+        print("The BMW costs around $30,000 ~ $100,000 depending on the model. ")
+    def type():
+        print("The BMW is a sports car")
+
+class Lamborghini():
+    def cost():
+        print("A Lamborghini costs $200,000 ~ $830,000,000 depending on the model. ")
+    def type():
+        print("A Lamborghini is a sports car. ")
+
+BMW = BMW()
+Lambo = Lamborghini()
+
+BMW.cost()
+BMW.type()
+
+Lambo.cost()
+Lambo.type()
